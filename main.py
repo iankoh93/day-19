f57a9cc3eabc4ac2c9d145a98c83ad7e7f43f0f8
iankoh93 <ian_koh93@hotmail.com>
2022-02-19 15:35:32 -0800

    Turtle races complete

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
 from turtle import Turtle, Screen
+import random as r
 
-tim = Turtle()
+competitor_list = []
+colors = ["red", "blue", "green", "orange", "grey", "black", "pink", "silver", "gold"]
+screen_width = 600
+screen_height = 900
+TURTLE_SIZE = 2
+
+is_race_on = False
 screen = Screen()
+screen.setup(width=screen_width, height=screen_height)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ").lower()
+
+while user_bet not in colors:
+    user_bet = screen.textinput(title="Make your bet", prompt=f"That color is not in the race, enter another color from the list below!\n {' '.join(colors)}: ").lower()
+
+if user_bet:
+    is_race_on = True
 
 
-def move_forwards():
-    tim.forward(10)
+def create_turtles():
+    for i in range(0, len(colors)):
+        color_turtle = Turtle("turtle")
+        color_turtle.penup()
+        color_turtle.turtlesize(TURTLE_SIZE)
+        color_turtle.color(colors[i])
+        competitor_list.append(color_turtle)
 
 
-def turn_left():
-    tim.left(10)
+def starting_position(width, height):
+    num_compete = len(competitor_list)
+    start = width/2 * -1 + 30
+    spacing = height / (num_compete + 1)
+    start_y = height/2
+    for i in range(0, len(competitor_list)):
+        start_y -= spacing
+        competitor_list[i].goto(x=start, y=start_y)
 
 
-def turn_right():
-    tim.right(10)
+def racing(turtle):
+    random_distance = r.randint(1, 10)
+    turtle.forward(random_distance)
 
 
-def move_backwards():
-    tim.forward(-10)
+create_turtles()
+starting_position(width=screen_width, height=screen_height)
 
 
-screen.listen()
-screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="a", fun=turn_left)
-screen.onkey(key="d", fun=turn_right)
-screen.onkey(key="s", fun=move_backwards)
+while is_race_on:
+    for i in range(0, len(competitor_list)):
+        racing(competitor_list[i])
+        if competitor_list[i].xcor() >= screen_width/2 - (15*TURTLE_SIZE):
+            is_race_on = False
+            winning_color = competitor_list[i].fillcolor()
+
+if winning_color == user_bet:
+    print(f"Congratulations your {user_bet} turtle won!")
+else:
+    print(f"Sorry the {winning_color} turtle won. You lost!")
+
 screen.exitonclick()
